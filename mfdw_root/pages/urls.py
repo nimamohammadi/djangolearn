84aaaf482fa52cb0b66014de8495976ee5306519
unknown <nimamhd@gmail.com>
2019-04-22 22:44:54 +0430

    import forms

--- mfdw_root/pages/urls.py
+++ mfdw_root/pages/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # path('', views.index, name='index'),
     path('', views.index, {'pagename': ''}, name='home'),
+    path('contact', views.contact, name='contact'),
     path('<str:pagename>', views.index, name='index'),
 
 ]
