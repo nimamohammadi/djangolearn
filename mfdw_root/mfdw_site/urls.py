e430f37c3b8fd34565ab1ac2e03f8e8e34b7640d
unknown <nimamhd@gmail.com>
2019-04-25 14:58:07 +0430

    forms

--- mfdw_root/mfdw_site/urls.py
+++ mfdw_root/mfdw_site/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from quotes import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pages.urls'))
+    path('quote/', include('quotes.urls')),
+    path('', include('pages.urls')),
 ]
